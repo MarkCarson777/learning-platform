d844c6ffa0297e69e7ee38a03e1debb3561548bd
Mark Carson <markcarson121@gmail.com>
2026-04-29 18:59:33 +0100

    added generic button component with minimum boilerplate

---
+++ src/components/ui/Button/index.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./index";
+
+const meta = {
+  title: "UI/Button",
+  component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
