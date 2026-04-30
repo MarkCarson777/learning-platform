cb7b14034520ca2cc7a5ebfcec60d7b8377f2fbd
Mark Carson <markcarson121@gmail.com>
2026-04-30 09:53:20 +0100

    rendered json data in library

--- src/routes/_protected/library/index.tsx
+++ src/routes/_protected/library/index.tsx
@@ -1,5 +1,19 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useModules } from "../../../hooks/useModules";
 
 export const Route = createFileRoute("/_protected/library/")({
-  component: () => <div>Library</div>,
+  component: LibraryPage,
 });
+
+function LibraryPage() {
+  const { data: modules, isLoading } = useModules();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <>
+      <h1>Library</h1>
+      <pre>{JSON.stringify(modules, null, 2)}</pre>
+    </>
+  );
+}
