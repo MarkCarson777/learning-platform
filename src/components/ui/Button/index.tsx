a33ee9420de5b4b4e4ae662ece01b45ffab305a5
Mark Carson <markcarson121@gmail.com>
2026-05-01 20:10:47 +0100

    added basic styling to Button and fixed tailwind import for storybook

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -4,5 +4,12 @@ interface ButtonProps {
 }
 
 export const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
-  return <button onClick={onClick}>{children}</button>;
+  return (
+    <button
+      className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 cursor-pointer"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 };
